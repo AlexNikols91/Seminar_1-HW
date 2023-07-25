51ef838be1833697619250edc86515a17813c27c
Aleksey <alexei.nikolaeff2017@yandex.ru>
2023-07-25 15:47:20 +0400

    Домашнее задание к 1 семинару

---
+++ Seminar_1(HW)/Program.cs
@@ -0,0 +1,91 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int number2=Convert.ToInt32(Console.ReadLine());
+
+// if(number1>number2)
+// {
+//     Console.WriteLine(number1);
+// }
+// else
+// {
+//     Console.WriteLine(number2);
+// }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int number2=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int number3=Convert.ToInt32(Console.ReadLine());
+
+// if (number1>number2);
+// {
+//     if(number1>number3)
+//     {
+//         Console.WriteLine(number1);
+//     }
+//     else
+//     {
+//         Console.WriteLine(number3);
+//     }
+// }    
+
+// else if(number2>number3)
+// {
+//     Console.WriteLine(number2);
+// }
+// else
+// {
+//     Console.WriteLine(number3);
+// } (Выдает ошибки, я не совсем понял почему...)
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число");
+// int number=Convert.ToInt32(Console.ReadLine());
+
+// if(number%2==0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+
+//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число");
+// int number=Convert.ToInt32(Console.ReadLine());
+
+// int numberEven=(numberEven%2==0);
+
+// while(numberEven<=10);
+// {
+//     Console.WriteLine(numberEven);
+//     numberEven=numberEven+2;
+// } (Здесь тоже что - то пошло не так, я просто не знаю как это записать)
