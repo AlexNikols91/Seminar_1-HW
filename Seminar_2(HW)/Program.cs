1edd3f6dd258f8e68e4c2e1be3a608d0d992628f
Aleksey <alexei.nikolaeff2017@yandex.ru>
2023-07-31 20:47:54 +0400

    Домашка ко второму семинару

---
+++ Seminar_2(HW)/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int num = new Random().Next(100,1000);
+// Console.WriteLine(num);
+
+// int num1 = num / 100;
+// int num2 = num / 10;
+// int result = num2 - num1*10;
+
+// Console.WriteLine(result);
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// int num = new Random().Next(10,40000);
+// Console.WriteLine(num);
+// int num1 = num / 1000;
+// int num2 = num / 100;
+// int num3 = num / 10;
+// int result = num2 - num1*10;
+// int result2 = num3 - num2*10;
+// if(num>10000)
+// {
+//     Console.WriteLine(result);
+// }
+// else if((num>99)&&(num<10000))
+// {
+//     Console.WriteLine(result2);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// int num = new Random().Next(1,8);
+// Console.WriteLine(num);
+
+// if(num>5)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+
+
+
