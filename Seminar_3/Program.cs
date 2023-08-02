efc6224c74076f8b3b865cb3eb811620f346a35c
Aleksey <alexei.nikolaeff2017@yandex.ru>
2023-08-02 20:04:00 +0400

    Работа на 3 семинаре

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,123 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// Console.WriteLine("Введите число");
+// int X = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int Y = Convert.ToInt32(Console.ReadLine());
+
+// if(X>0 && Y>0)
+// {
+//     Console.WriteLine("1");
+// }
+// else if(X<0 && Y>0)
+// {
+//     Console.WriteLine("2");
+// }
+// else if(X<0 && Y<0)
+// {
+//     Console.WriteLine("3");
+// }
+// else if(X>0 && Y<0)
+// {
+//     Console.WriteLine("4");
+// }
+// else
+// {
+//     Console.WriteLine("Точка лежит на оси");
+// }
+
+
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("Введите номер четверти");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num == 1)
+// {
+//     Console.WriteLine("X и Y больше 0");
+// }
+// else if(num == 2)
+// {
+//     Console.WriteLine("X меньше 0, а Y больше 0");
+// }
+// else if(num == 3)
+// {
+//     Console.WriteLine("X и Y меньше 0");
+// }
+// else if(num == 4)
+// {
+//     Console.WriteLine("X больше 0, а Y меньше 0");
+// }
+// else
+// {
+//     Console.WriteLine("Пожалуйста, введите номер четверти от 1 до 4");
+// }
+
+
+// Console.WriteLine("Введите номер четверти");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// switch(num)
+// {
+//     case 1:
+//     {
+//         Console.Write("X>0 Y>0");
+//         break;
+//     }
+//     case 2:
+//     {
+//         Console.Write("X<0 Y>0");
+//         break;
+//     }
+//     case 3:
+//     {
+//         Console.Write("X<0 Y<0");
+//         break;
+//     }
+//     case 4:
+//     {
+//         Console.Write("X>0 Y<0");
+//         break;
+//     }
+//     default:
+//     {
+//         Console.Write("Не подходит");
+//         break;
+//     }
+// }
+
+
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+// Console.WriteLine("Введите число");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+
+// double s = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+
+// System.Console.WriteLine(s);
+
+
+// Напишите программу, которая принимает на вход число(N) 
+// и выдает таблицу квадратов чисел от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for(int i= 1; i<=num; i++)
+// {
+//     Console.WriteLine(Math.Pow(i,2));
+// }
+
+
